990ad54311959e3f6332827641843fd608563ccf
Trashness <kompaniets17@mail.ru>
2023-02-08 15:49:49 +0500

    11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default  function App() {
+	const  questions= [
+		{
+			questionText: 'Столица РФ?',
+			answerOptions: [
+				{ answerText: 'Токио', isCorrect: false },
+				{ answerText: 'Лондон', isCorrect: false },
+				{ answerText: 'Париж', isCorrect: false },
+				{ answerText: 'Москва', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Какая компания выпускает Iphone?',
+			answerOptions: [
+				{ answerText: 'Tesla', isCorrect: false },
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Blizzard', isCorrect: false },
+				{ answerText: 'Stark ind.', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Какой сейчас год?',
+			answerOptions: [
+				{ answerText: '2021', isCorrect: false },
+				{ answerText: '2022', isCorrect: false },
+				{ answerText: '2023', isCorrect: true },
+				{ answerText: '2024', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Сколько человек должен спать?',
+			answerOptions: [
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: false },
+				{ answerText: '8', isCorrect: true },
+				{ answerText: '9', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Самое высокое здание в мире?',
+			answerOptions: [
+				{ answerText: '565', isCorrect: false },
+				{ answerText: '790', isCorrect: false },
+				{ answerText: '828', isCorrect: true },
+				{ answerText: '1000', isCorrect: false },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					Вы набрали {score} из {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
